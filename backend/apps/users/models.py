56b57815b7d766e8c074c2a46311514771cadf79
ZeeshanHabibAnsari <garammasala574@gmail.com>
2026-07-09 21:59:16 +0530

    fix: enforce unique email constraint and update verify view

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False, null=False)
     ROLE_CHOICES = (('Student', 'Student'), ('Admin', 'Admin'))
     full_name = models.CharField(max_length=100, blank=True, null=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Student')
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -253,9 +253,7 @@ class VerifyEmailView(APIView):
             return Response({"error": "Email and OTP are required"}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            user = User.objects.filter(email=email, is_active=False).order_by('-id').first()
-            if not user:
-                return Response({"error": "No inactive user found with this email"}, status=status.HTTP_404_NOT_FOUND)
+             user = User.objects.get(email=email)
 
         except User.DoesNotExist:
             return Response({"error": "No user found"}, status=status.HTTP_404_NOT_FOUND)
