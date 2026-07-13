0c7352efa9de103705333610e9981103780fb4f6
ZeeshanHabibAnsari <garammasala574@gmail.com>
2026-07-13 19:22:26 +0530

    fix: clean indentation and separate serializers for active attempts and review

--- backend/apps/questions/serializers.py
+++ backend/apps/questions/serializers.py
@@ -1,40 +1,23 @@
 from rest_framework import serializers
 from .models import Question, QuestionOption
 
-class QuestionOptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QuestionOption
-        fields = ['id', 'option_text', 'is_correct']
-
 class QuestionSerializer(serializers.ModelSerializer):
-    options = QuestionOptionSerializer(many=True, source='questionoption_set', required=False)
+    options = serializers.SerializerMethodField()
 
     class Meta:
         model = Question
-        fields = ['id', 'quiz', 'question_text', 'question_type', 'marks', 'question_order', 'options']
-        read_only_fields = ['quiz']  
+        fields = ['id', 'question_text', 'question_type', 'marks', 'question_order', 'options']
 
-    def create(self, validated_data):
-        options_data = validated_data.pop('questionoption_set', [])
-        question = Question.objects.create(**validated_data)
-        for option_data in options_data:
-            QuestionOption.objects.create(question=question, **option_data)
-        return question
+    def get_options(self, obj):
+        return [
+            {
+                "id": opt.id,
+                "option_text": opt.option_text,
+                "is_correct": opt.is_correct
+            }
+            for opt in obj.questionoption_set.all().order_by('id')
+        ]
 
-    def update(self, instance, validated_data):
-        options_data = validated_data.pop('questionoption_set', [])
-        instance.question_text = validated_data.get('question_text', instance.question_text)
-        instance.question_type = validated_data.get('question_type', instance.question_type)
-        instance.marks = validated_data.get('marks', instance.marks)
-        instance.question_order = validated_data.get('question_order', instance.question_order)
-        instance.save()
-
-        # Purani options hatao aur nayi daalo (simple approach)
-        instance.questionoption_set.all().delete()
-        for option_data in options_data:
-            QuestionOption.objects.create(question=instance, **option_data)
-        return instance
-    
 class AttemptQuestionSerializer(serializers.ModelSerializer):
     options = serializers.SerializerMethodField()
 
@@ -42,8 +25,7 @@ class AttemptQuestionSerializer(serializers.ModelSerializer):
         model = Question
         fields = ['id', 'question_text', 'question_type', 'marks', 'options']
 
-     def get_options(self, obj):
-        
+    def get_options(self, obj):
         return [
             {"id": opt.id, "text": opt.option_text}
             for opt in obj.questionoption_set.all().order_by('id')
