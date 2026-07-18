0dda01991d4a31426e180125367df66fb35aaef9
ZeeshanHabibAnsari <garammasala574@gmail.com>
2026-07-18 23:02:27 +0530

    fix: switch email to SendGrid (fixes SMTP error on Render)

--- backend/quiz_project/settings.py
+++ backend/quiz_project/settings.py
@@ -89,6 +89,7 @@ ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL', FROM_EMAIL)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'apikey'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
